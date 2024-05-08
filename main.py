cf6adf332c7b5b7926bdbb2e03e4ed3b2cf42caa
João Vitor Moraes <joaovitormoraes2008@hotmail.com>
2024-05-08 16:57:01 +0100

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,17 @@
+print("Jornada de dados")
+
+#Variáveis
+idade = 24 
+nome = "João Vitor Moraes"
+
+#print
+print(idade)
+
+#soma
+nova_idade = idade + 1
+
+#lista
+lista = [1, 2, 3, 4]
+
+#metodos
+lista_invertida = lista.reverse()
